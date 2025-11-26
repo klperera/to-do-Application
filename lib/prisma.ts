260406454972d107f2ac2219a61fd121d65dcb5f
klperera <klperera660@gmail.com>
2025-11-26 14:18:25 +0530

    refactor: optimize Prisma client initialization and global variable declaration

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,6 +2,13 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../prisma/generated/prisma/client";
 
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
+const prisma = global.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
 export default prisma;
